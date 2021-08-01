458814a5ed61f46e04aa89f43978923c9b143df3
Heemaeng <gml7814@gmail.com>
2021-08-02 01:08:08 +0900

    리스트 아이템

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Header from './components/Header';
+import TodoItem from './components/TodoItem';
 
 export default class App extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Header />
+        <TodoItem title="리액트 네이티브 앱 만들기" done={true}/>
+        <TodoItem title="스터디 진행하기" done={false}/>
       </SafeAreaView>
     );
   }
